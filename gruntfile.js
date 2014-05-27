3eab826bfad84ecaf23c1ece494092911d4dfd27
Luke Wilde <luke.wilde@clock.co.uk>
2014-05-27 11:55:55 +0100

    init.

---
+++ gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function (grunt) {
+  grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.loadNpmTasks('grunt-contrib-connect')
+  grunt.loadNpmTasks('grunt-open')
+  grunt.loadNpmTasks('grunt-contrib-concat')
+
+  grunt.initConfig(
+    { pkg: grunt.file.readJSON('package.json')
+    , connect:
+      { server:
+        { options:
+          { port: 8080
+          , base: './deploy'
+          }
+        }
+      }
+    , concat:
+      { dist:
+        { src:
+          [ 'src/lib/**/*.js'
+          , 'src/game/**/*.js'
+          ]
+        , dest: 'deploy/js/<%= pkg.name %>.js'
+        }
+      }
+    , watch:
+      { files: 'src/**/*.js'
+      , tasks: ['concat']
+      }
+    , open:
+      { dev:
+        { path: 'http://localhost:8080/index.html'
+        }
+      }
+    }
+  )
+
+  grunt.registerTask('default', ['concat', 'grunt-contrib-connect', 'open', 'watch'])
+}
