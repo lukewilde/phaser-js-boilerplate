4a34123c8a7a68b39bb7a32590d4fb43bd6a433b
Luke Wilde <luke.wilde@clock.co.uk>
2014-05-29 11:35:02 +0100

    Fixing source maps.

--- gruntfile.js
+++ gruntfile.js
@@ -84,7 +84,7 @@ module.exports = function (grunt) {
           { transform: ['browserify-shim']
           , require: shims
           , bundleOptions:
-            { debug: productionBuild
+            { debug: !productionBuild
             }
           }
         }
@@ -93,7 +93,7 @@ module.exports = function (grunt) {
         , dest: '<%= project.dest %>/app.js'
         , options:
           { bundleOptions:
-            { debug: productionBuild
+            { debug: !productionBuild
             }
           , external: shims
           }
