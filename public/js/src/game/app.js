16190f234c92935af76224c73a773fb586a73b28
Luke Wilde <luke.wilde@clock.co.uk>
2014-06-03 12:44:37 +0100

    Correcting an initing boot.

--- public/js/src/game/app.js
+++ public/js/src/game/app.js
@@ -1,5 +1,7 @@
 var Phaser = require('Phaser')
   , game = new Phaser.Game(800, 600, Phaser.AUTO, '')
-  , boot = require('./screens/boot.js')
+  , boot = require('./states/boot.js')
 
-  game.state.add('boot', boot(game))
+game.state.add('boot', boot(game))
+
+game.state.start('boot')
