f0f3da56dfd1d08360173ced8b9b9900f689cff0
Luke Wilde <luke.wilde@clock.co.uk>
2014-07-03 13:25:44 +0100

    Initialising Phaser in the correct DOM element.

--- src/js/game/app.js
+++ src/js/game/app.js
@@ -1,7 +1,7 @@
 var Phaser = require('Phaser')
   , properties = require('./properties')
   , boot = require('./states/boot.js')
-  , game = new Phaser.Game(properties.size.x, properties.size.y, Phaser.AUTO, '')
+  , game = new Phaser.Game(properties.size.x, properties.size.y, Phaser.AUTO, 'game')
 
 game.state.add('boot', boot(game))
 
