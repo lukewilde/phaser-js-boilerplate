d5a6aa7b8c0e5c581bdf072b8307551ffec8cb82
Luke Wilde <luke.wilde@clock.co.uk>
2014-07-06 13:02:12 +0100

    Ensuring requires are at the top of the file.

--- src/js/game/app.js
+++ src/js/game/app.js
@@ -1,11 +1,11 @@
 var Phaser = require('Phaser')
   , properties = require('./properties')
-  , game = new Phaser.Game(properties.size.x, properties.size.y, Phaser.AUTO, 'game')
   , states =
     { boot: require('./states/boot.js')
     , preloader: require('./states/preloader.js')
     , game: require('./states/game.js')
-    };
+    }
+  , game = new Phaser.Game(properties.size.x, properties.size.y, Phaser.AUTO, 'game');
 
 game.state.add('boot', states.boot(game));
 game.state.add('preloader', states.preloader(game));
