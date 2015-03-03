9ab2c7e43817a39d661aa6f43b4b49c22c584494
Luke Wilde <luke.wilde@clock.co.uk>
2015-03-03 12:22:56 +0000

    Lint.

--- src/js/game/states/game.js
+++ src/js/game/states/game.js
@@ -2,22 +2,25 @@ var Phaser = require('Phaser');
 
 module.exports = function(game) {
 
-  var gameState = new Phaser.State();
-  var logo = null;
+  var gameState = new Phaser.State()
+    , logo = null;
 
   gameState.create = function () {
-    logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+
     game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
     logo.anchor.setTo(0.5, 0.5);
+
     game.physics.arcade.enable(logo);
   };
 
   gameState.update = function () {
-    console.log('update')
+    console.log('update');
+  };
 
-  }
   gameState.render = function () {
-    console.log('render')
+    console.log('render');
     game.debug.body(logo);
   };
 
